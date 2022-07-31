0b14c9d88722cbb47b1cbb42c234885cda8998e9
marykidun <107813028+marykidun@users.noreply.github.com>
2022-08-01 00:16:41 +0300

    Added new component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from "react";
 import Weather from "./Weather";
+import "bootstrap/dist/css/bootstrap.css";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Weather Lab</h1>
-      <Weather />
-
-      <p>
-        <a
-          href="https://github.com/marykidun"
-          target="_blank"
-          rel="noreferrer"
-          title="My Github"
-        >
-          Open-source
-        </a>{" "}
-        by Maryna Kidun.
-      </p>
+      <div className="container">
+        <h1>Weather Lab</h1>
+        <Weather defaultCity="Valencia" />
+      </div>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,56 +1,95 @@
 import React, { useState } from "react";
+import FormattedDate from "./FormattedDate";
 import axios from "axios";
 
-export default function WeatherSearch() {
-  const [city, setCity] = useState("");
-  const [loaded, setLoaded] = useState(false);
-  const [weather, setWeather] = useState({});
+import "./Weather.css";
 
-  function displayWeather(response) {
-    setLoaded(true);
-    setWeather({
+export default function WeatherSearch(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
       temperature: response.data.main.temp,
       wind: response.data.wind.speed,
+      city: response.data.name,
       humidity: response.data.main.humidity,
-      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       description: response.data.weather[0].description,
+      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png",
+      date: new Date(response.data.dt * 1000),
     });
   }
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    let apiKey = "094780c710fa4efd669f0df8c3991927";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(displayWeather);
-  }
-
-  function updateCity(event) {
-    setCity(event.target.value);
-  }
-
-  let form = (
-    <form onSubmit={handleSubmit}>
-      <input type="search" placeholder="Enter a city.." onChange={updateCity} />
-      <button type="Submit">Search</button>
-    </form>
-  );
-
-  if (loaded) {
+  if (weatherData.ready) {
     return (
-      <div>
-        {form}
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter your city"
+                className="form-control"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-info w-100"
+              />
+            </div>
+          </div>
+        </form>
+        <h2>Valencia</h2>
         <ul>
-          <li>Temperature: {Math.round(weather.temperature)}°C</li>
-          <li>Description: {weather.description}</li>
-          <li>Humidity: {weather.humidity}%</li>
-          <li>Wind: {weather.wind}km/h</li>
           <li>
-            <img src={weather.icon} alt={weather.description} />
+            <FormattedDate date={weatherData.date} />
           </li>
+          <li className="text-capitalize">{weatherData.description}</li>
         </ul>
+
+        <div className="row mt-3">
+          <div className="col-6">
+            <img
+              src={weatherData.iconUrl}
+              alt={weatherData.description}
+              className="float-left"
+            />
+
+            <span className="temperature">
+              {Math.round(weatherData.temperature)}
+            </span>
+            <span className="unit"> C</span>
+          </div>
+
+          <div className="col-6">
+            <ul>
+              <li>Perticipation: 100%</li>
+              <li>Humidity: {weatherData.humidity} %</li>
+              <li>Wind: {weatherData.wind} m/s</li>
+            </ul>
+          </div>
+        </div>
+        <footer>
+          <a
+            href="https://github.com/marykidun"
+            target="_blank"
+            rel="noreferrer"
+            title="My Github"
+          >
+            Open-source
+          </a>{" "}
+          by Maryna Kidun.
+        </footer>
       </div>
     );
   } else {
-    return form;
+    const apiKey = "4a373ca426c2a1fcd667e2103b2b6b9f";
+
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
   }
 }
